fc4dc4d0f8db1d82f8692e81c2445ddb436ede76
Jordan Schmelzer <jordanschmelzer@gmail.com>
2025-08-24 11:45:32 -0500

    working on exp

--- my-portfolio-app/src/app/components/experience/experience.component.ts
+++ my-portfolio-app/src/app/components/experience/experience.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-experience',
@@ -9,49 +10,113 @@ import { CommonModule } from '@angular/common';
 })
 export class ExperienceComponent {
   public jobs: Record<string, string>[] = [];
+  public expanded: boolean[] = [];
+  public totalYearsExp: number = 0;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     // TODO: Load Jobs from backend? Service? This is fine for now.
-
-    const job1: Record<string, string> = {
-      companyName: "S&C Electric",
-      jobTitle: "Operations Technology Engineer",
-      description: `
-      I work here!
-      Second line
-      `
+    const dateTimeFormatOpts: Intl.DateTimeFormatOptions = {
+      month: 'short', year: 'numeric'
     };
 
-    const job2: Record<string, string> = {
-      companyName: "S&C Electric",
-      jobTitle: "Operations Technology Engineer",
-      description: `
-      I work here!
-      Second line
-      `
-    };
+    this.http.get('text/jobs/sandc.txt', { responseType: 'text' })
+      .subscribe(data => {
+        const startDate: Date = new Date("2024-07-01T00:00:00");
+        const endDate: Date = new Date();
+        const difInMilliSec = Math.abs(startDate.getTime() - endDate.getTime());
+        const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+        var jobDuration: number = Math.round((difInMilliSec / msInYear) * 100) / 100;
 
-    const job3: Record<string, string> = {
-      companyName: "S&C Electric",
-      jobTitle: "Operations Technology Engineer",
-      description: `
-      I work here!
-      Second line
-      `
-    };
+        const job: Record<string, any> = {
+          companyName: "S&C Electric Company",
+          jobTitle: "Operations Technology Engineer",
+          description: data,
+          startDate: startDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          endDate: endDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          duration: jobDuration,
+          current: true
+        };
+        this.jobs.push(job)
+        this.totalYearsExp += jobDuration;
+      })
 
-    const job4: Record<string, string> = {
-      companyName: "S&C Electric",
-      jobTitle: "Operations Technology Engineer",
-      description: `
-      I work here!
-      Second line
-      `
-    };
+    this.http.get('text/jobs/plexus.txt', { responseType: 'text' })
+      .subscribe(data => {
+        const startDate: Date = new Date("2020-06-01T00:00:00");
+        const endDate: Date = new Date("2024-07-01T00:00:00");
+        const difInMilliSec = Math.abs(startDate.getTime() - endDate.getTime());
+        const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+        var jobDuration: number = Math.round((difInMilliSec / msInYear) * 100) / 100;
 
-    this.jobs.push(job1);
-    this.jobs.push(job2);
-    this.jobs.push(job3);
-    this.jobs.push(job4);
+        const job: Record<string, any> = {
+          companyName: "Plexus Corporation",
+          jobTitle: "Manufacturing System Engineer",
+          description: data,
+          startDate: startDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          endDate: endDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          duration: jobDuration,
+          current: false
+        };
+        this.jobs.push(job);
+        this.totalYearsExp += jobDuration;
+      })
+
+    this.http.get('text/jobs/husco.txt', { responseType: 'text' })
+      .subscribe(data => {
+        const startDate: Date = new Date("2019-04-01T00:00:00");
+        const endDate: Date = new Date("2020-06-01T00:00:00");
+        const difInMilliSec = Math.abs(startDate.getTime() - endDate.getTime());
+        const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+        var jobDuration: number = Math.round((difInMilliSec / msInYear) * 100) / 100;
+
+        const job: Record<string, any> = {
+          companyName: "Husco International Inc.",
+          jobTitle: "Advanced Manufacturing Engineer",
+          description: data,
+          startDate: startDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          endDate: endDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          duration: jobDuration,
+          current: false
+        };
+        this.jobs.push(job)
+        this.totalYearsExp += jobDuration;
+      })
+
+    this.http.get('text/jobs/greenheck.txt', { responseType: 'text' })
+      .subscribe(data => {
+        const startDate: Date = new Date("2016-01-01T00:00:00");
+        const endDate: Date = new Date("2016-09-01T00:00:00");
+        const difInMilliSec = Math.abs(startDate.getTime() - endDate.getTime());
+        const msInYear = 1000 * 60 * 60 * 24 * 365.25;
+        var jobDuration: number = Math.round((difInMilliSec / msInYear) * 100) / 100;
+
+        const job: Record<string, any> = {
+          companyName: "Greenheck Fan Company",
+          jobTitle: "Product Development Coop",
+          description: data,
+          startDate: startDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          endDate: endDate.toLocaleDateString('en-US', dateTimeFormatOpts),
+          duration: jobDuration,
+          current: false
+        };
+        this.jobs.push(job)
+        this.totalYearsExp += jobDuration;
+      })
+
+    // Calculate total experience in years
+    // this will be 0 b/c async http calls. cheated and just put calc in async method
+    // this.totalYearsExp += Number(job["duration"])
+
+    //this.jobs.push(job1, job2, job3, job4)
+    this.expanded = this.jobs.map(() => false); // show more toggle map
   }
+
+  toggle(index: number): void {
+    this.expanded[index] = !this.expanded[index];
+  }
+
+  get roundedYearsExp(): number {
+    return Math.round(this.totalYearsExp);
+  }
+
 }
